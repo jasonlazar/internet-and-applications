28a7b796e32ee27bc15238d60874d570900fa2be
Ιάσονας Λαζαρίδης <jasonlaz98@yahoo.gr>
2020-08-04 12:59:27 +0300

    Remove unused imports

--- appathon/src/main/java/com/ntua/appathon/services/services.java
+++ appathon/src/main/java/com/ntua/appathon/services/services.java
@@ -1,10 +1,5 @@
 package com.ntua.appathon.services;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
